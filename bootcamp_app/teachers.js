10458004f8b28b964bc965777156498d032f040f
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-10-14 17:09:13 +0000

    Fixed

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,9 +13,9 @@ const values = [`%${cohortName}%`];
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
-JOIN students ON assistance_requests.student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
 ORDER BY teacher;
 `, values)
